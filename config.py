bcfea63e3a801e43c7a37fe96dddcb8f3a017abe
arslan.akautdinov <arslan.akautdinov@rt.ru>
2020-07-23 19:27:15 +0300

    fix host

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ DIR_VIDEO = os.path.join(DIR_ROOT, "videos")
 BIN_FFMPEG = "ffmpeg"
 BIN_FFPROBE = "ffprobe"
 
-HOST = "10.50.6.127"
-PORT = 21554
+# mashine ip
+HOST = os.environ.get("VIDEO_HOSTING")
 
+PORT = 21554
 RANGE_START = 10000
 RANGE_END = 11000
